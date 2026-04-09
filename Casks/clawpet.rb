c5cfd05e1bac728df1d052f64e19ef374a5a92da
auk <auk@littlelights.ai>
2026-04-09 19:18:08 +0800

    chore(cask): release clawpet 0.1.0-alpha.2

--- Casks/clawpet.rb
+++ Casks/clawpet.rb
@@ -1,5 +1,5 @@
 cask "clawpet" do
-  version "0.1.0-alpha.1"
+  version "0.1.0-alpha.2"
   sha256 "888b0b75ed1659edf82c262ffcab6012ee9c09794025f2286fb0472524ebcb31"
 
   url "https://crazyrope-backup.oss-cn-shanghai-internal.aliyuncs.com/app/clawpet/ios/20260408105646/Clawpet_China_0.1.0_aarch64.dmg"
